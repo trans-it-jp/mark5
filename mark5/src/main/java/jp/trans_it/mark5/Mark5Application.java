c23b7a71ea9eba4cdf7dc71504a9352667d96043
Satoshi Tanaka <satstnka@gmail.com>
2020-07-19 15:38:23 +0900

    Created project.

---
+++ mark5/src/main/java/jp/trans_it/mark5/Mark5Application.java
@@ -0,0 +1,13 @@
+package jp.trans_it.mark5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mark5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mark5Application.class, args);
+	}
+
+}
---
+++ mark5/src/test/java/jp/trans_it/mark5/Mark5ApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.trans_it.mark5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mark5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
